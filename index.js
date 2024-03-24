936b91ff8e0723e2083501d300d3fca0c56bfd98
R-fave <techclime1@gmail.com>
2024-03-24 13:15:39 +0100

    main

---
+++ index.js
@@ -0,0 +1,9 @@
+const services = document.getElementById('porto')
+const porto = document.getElementById("proto-dd")
+
+
+
+services.addEventListener("mouseover", function(){
+    document.getElementById("proto-div").innerHTML = '<div class="porto-div" id="porto-div"><p>Domestic logistics</p><p>Domestic logistics</p><p>Domestic logistics</p><p>Domestic logistics</p><p>Domestic logistics</p></div>'
+})
+
